b39d443854e1cd5e005a29984fccdf29541fcfbd
Emilio Pavia <emilio.pavia@gmail.com>
2014-12-11 16:40:06 +0100

    Initial Commit

---
+++ Ganalytics/Ganalytics.h
@@ -0,0 +1,13 @@
+//
+//  Ganalytics.h
+//  Ganalytics
+//
+//  Created by Emilio Pavia on 11/12/14.
+//  Copyright (c) 2014 Emilio Pavia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ganalytics : NSObject
+
+@end
---
+++ Ganalytics/Ganalytics.m
@@ -0,0 +1,13 @@
+//
+//  Ganalytics.m
+//  Ganalytics
+//
+//  Created by Emilio Pavia on 11/12/14.
+//  Copyright (c) 2014 Emilio Pavia. All rights reserved.
+//
+
+#import "Ganalytics.h"
+
+@implementation Ganalytics
+
+@end
